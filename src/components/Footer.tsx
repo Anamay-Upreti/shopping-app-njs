141c4d4e953dca6877ee8af367af71fdac9f950a
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-05-23 23:50:35 +0530

    added-footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black text-gray-500 text-center py-4 pb-16 md:pb-4'>Copyright 0 Logo <br /> All rights Reserved 2023</div>
+  )
+}
+
+export default Footer
